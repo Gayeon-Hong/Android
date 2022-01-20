a47d64bcdc7a2f5244b3e57b74cc9a9ac9c6b5e5
Drinkmate <93218452+Drinkmate@users.noreply.github.com>
2022-01-21 02:34:40 +0900

    ㅎㅎ

---
+++ app/src/main/java/com/example/body_care/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.body_care;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    ImageView btnChatbot, btnScan, btnGym;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnChatbot = (ImageView) findViewById(R.id.chat_bot);
+        btnScan = (ImageView) findViewById(R.id.scan);
+        btnGym = (ImageView) findViewById(R.id.gym);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGym.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Workout_List.class);
+                startActivity(intent);
+            }
+        });
+
+        btnChatbot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
